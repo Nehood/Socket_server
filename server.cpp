f8b9bfc3a65efe6ebfc305d84753ede5845748c1
Nehood <gorkiewiczm@gmail.com>
2018-03-01 21:30:48 +0100

    initial version

---
+++ server.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+#define PORT 5555
+
+int main()
+{
+	WSADATA WSAData;
+	SOCKET server, client;
+	SOCKADDR_IN serverAddr, clientAddr;
+
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
+	server = socket(AF_INET, SOCK_STREAM, 0);
+
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+
+	bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+	listen(server, 0);
+
+	cout << "Listening for incoming connections..." << endl;
+
+	char buffer[1024];
+	int clientAddrSize = sizeof(clientAddr);
+	if ((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+	{
+		cout << "Client connected!" << endl;
+		recv(client, buffer, sizeof(buffer), 0);
+		cout << "Client says: " << buffer << endl;
+		memset(buffer, 0, sizeof(buffer));
+
+		closesocket(client);
+		cout << "Client disconnected" << endl;
+	}
+
+	cin.get();
+}
